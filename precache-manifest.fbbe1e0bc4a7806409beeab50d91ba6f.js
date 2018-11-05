205995b7e5c32f302fa463997985e0ee6a36e7a1
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2018-11-05 06:55:11 -0300

    initial commit

---
+++ precache-manifest.fbbe1e0bc4a7806409beeab50d91ba6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f073992db6a860365a6",
+    "url": "/static/js/main.4f073992.chunk.js"
+  },
+  {
+    "revision": "13872983948a0498dbb2",
+    "url": "/static/js/1.13872983.chunk.js"
+  },
+  {
+    "revision": "4f073992db6a860365a6",
+    "url": "/static/css/main.d4e4fd51.chunk.css"
+  },
+  {
+    "revision": "13872983948a0498dbb2",
+    "url": "/static/css/1.ddfd3c0d.chunk.css"
+  },
+  {
+    "revision": "dac2aa7e5093ab30c440ba9412516152",
+    "url": "/index.html"
+  }
+];
